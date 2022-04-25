adae73f40460b75a1fb9b7372339fbf4d816b92d
Zach <shaw.zacht@gmail.com>
2022-04-25 14:19:41 -0700

    all elements functioning, but 1 test failing

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,16 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewTaskForm() {
+function NewTaskForm({ categories, onTaskFormSubmit }) {
+
+  const [newTask, setNewTask] = useState({
+    text: "",
+    category: "Code"
+  })
+
+  function handleChange(e) {
+    setNewTask({
+      ...newTask,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  
+    const newItem = {
+      text: newTask.text,
+      category: newTask.category
+    }
+    onTaskFormSubmit(newItem)
+  }
+  
+  const dropdownCategories = categories.filter(category => {
+      if (category === "All") {
+      return false
+      } else {
+      return true
+    }
+  })
+
+  
+
   return (
-    <form className="new-task-form">
+    <form className="new-task-form" onSubmit={handleSubmit}>
       <label>
         Details
-        <input type="text" name="text" />
+        <input type="text" name="text" onChange={handleChange}/>
       </label>
       <label>
         Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
+        <select name="category" onChange={handleChange}>
+          {dropdownCategories.map(category => {
+           return <option key={category} value={category}>{category}</option>
+          })}
         </select>
       </label>
       <input type="submit" value="Add task" />
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,17 @@
 import React from "react";
+//import { TASKS } from "../data";
+import Task from "./Task"
 
-function TaskList() {
-  return (
+
+function TaskList({ tasksToDisplay, deleteTask }) {
+
+ console.log(tasksToDisplay)
+    
+  const displayTasks = tasksToDisplay.map((task) => <Task key={task.text} {...task} deleteTask={deleteTask}/>)   
+
+    return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {displayTasks}
     </div>
   );
 }
